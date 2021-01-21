80128100877dbd408f863088c3ddb4ce71fa3a3c
ISPserver <tjdals080@naver.com>
2021-01-21 17:58:30 +0900

    Rest Project with Android

---
+++ RestProject/src/main/java/com/koreait/restproject/model/member/service/MemberServiceImpl.java
@@ -0,0 +1,42 @@
+package com.koreait.restproject.model.member.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.koreait.restproject.model.domain.Member;
+import com.koreait.restproject.model.member.repository.MemberDAO;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+	@Autowired
+	private MemberDAO memberDAO;
+		
+	@Override
+	public List selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member select(int member_id) {
+		return null;
+	}
+
+	@Override
+	public void regist(Member member) throws RuntimeException{
+		memberDAO.insert(member);
+	}
+
+	@Override
+	public void update(Member member) {
+		
+	}
+
+	@Override
+	public void delete(int member_id) {
+		
+	}
+
+}
--- RestProject/src/main/java/com/koreait/restproject/rest/controller/member/MemberController.java
+++ RestProject/src/main/java/com/koreait/restproject/rest/controller/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.koreait.restproject.rest.controller.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,12 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.koreait.restproject.model.domain.Member;
+import com.koreait.restproject.model.member.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController //Controller에 ResponseBody가 탑재된 컨트롤러
 @Slf4j
 public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	
 	@GetMapping("/member")
 	@ResponseBody
 	public String getList() {
@@ -24,9 +29,12 @@ public class MemberController {
 	@PostMapping("/member")
 	public String regist(@RequestBody Member member) {
 		log.debug("Are u want regist?");
-		log.debug("m_id"+member.getM_id());
-		log.debug("m_pass"+member.getM_pass());
-		log.debug("m_name"+member.getM_name());
-		return "regist success";
+		log.debug("m_id: "+member.getM_id());
+		log.debug("m_pass: "+member.getM_pass());
+		log.debug("m_name: "+member.getM_name());
+		
+		memberService.regist(member);
+		
+		return "regist success";//REST에서는 개발자가 클라이언트에게 뭘 반환해야 할까?
 	}
 }
